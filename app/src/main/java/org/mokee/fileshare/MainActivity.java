88bca38da183a679230b746b53431521454ff267
XiNGRZ <chenxingyu92@gmail.com>
2019-08-26 15:06:06 +0800

    Create project

---
+++ app/src/main/java/org/mokee/fileshare/MainActivity.java
@@ -0,0 +1,17 @@
+package org.mokee.fileshare;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
