f30290fc27af847c773818119310dab505aa40f4
XiNGRZ <chenxingyu92@gmail.com>
2019-08-28 02:25:23 +0800

    Implement basic UI

--- app/src/main/java/org/mokee/fileshare/MainActivity.java
+++ app/src/main/java/org/mokee/fileshare/MainActivity.java
@@ -1,9 +1,17 @@
 package org.mokee.fileshare;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.ArrayMap;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import org.mokee.fileshare.airdrop.AirDropManager;
 
@@ -11,13 +19,22 @@ public class MainActivity extends AppCompatActivity implements AirDropManager.Ca
 
     private static final String TAG = "MainActivity";
 
+    private ArrayMap<String, String> mPeers = new ArrayMap<>();
+    private PeersAdapter mAdapter;
+
     private AirDropManager mAirDropManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mAirDropManager = new AirDropManager(this, this);
+
+        mAdapter = new PeersAdapter(this);
+
+        final RecyclerView peersView = findViewById(R.id.peers);
+        peersView.setAdapter(mAdapter);
     }
 
     @Override
@@ -35,11 +52,63 @@ public class MainActivity extends AppCompatActivity implements AirDropManager.Ca
     @Override
     public void onAirDropPeerFound(String id, String name) {
         Log.d(TAG, "Found: " + id + " (" + name + ")");
+        mPeers.put(id, name);
+        mAdapter.notifyDataSetChanged();
     }
 
     @Override
     public void onAirDropPeerDisappeared(String id) {
         Log.d(TAG, "Disappeared: " + id);
+        mPeers.remove(id);
+        mAdapter.notifyDataSetChanged();
+    }
+
+    private void handleItemClick(String id) {
+    }
+
+    private class PeersAdapter extends RecyclerView.Adapter<PeersAdapter.ViewHolder> {
+
+        private final LayoutInflater mInflater;
+
+        PeersAdapter(Context context) {
+            mInflater = LayoutInflater.from(context);
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new ViewHolder(mInflater.inflate(R.layout.item_peer, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            final String id = mPeers.keyAt(position);
+            final String name = mPeers.valueAt(position);
+            holder.nameView.setText(name);
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    handleItemClick(id);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mPeers.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+            TextView nameView;
+
+            ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                nameView = itemView.findViewById(R.id.name);
+            }
+
+        }
+
     }
 
 }
