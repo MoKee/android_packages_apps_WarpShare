3a45fbffb7bea1037c50297edfdbc670e754c699
XiNGRZ <chenxingyu92@gmail.com>
2019-08-28 10:31:38 +0800

    Pick a file to send

--- app/src/main/java/org/mokee/fileshare/MainActivity.java
+++ app/src/main/java/org/mokee/fileshare/MainActivity.java
@@ -1,7 +1,11 @@
 package org.mokee.fileshare;
 
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.util.ArrayMap;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,10 +24,14 @@ public class MainActivity extends AppCompatActivity implements AirDropManager.Ca
 
     private static final String TAG = "MainActivity";
 
+    private static final int REQUEST_PICK = 1;
+
     private ArrayMap<String, String> mPeers = new ArrayMap<>();
     private PeersAdapter mAdapter;
 
     private AirDropManager mAirDropManager;
+
+    private String mPeerPicked = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,11 +68,66 @@ public class MainActivity extends AppCompatActivity implements AirDropManager.Ca
     @Override
     public void onAirDropPeerDisappeared(String id) {
         Log.d(TAG, "Disappeared: " + id);
+
+        if (id.equals(mPeerPicked)) {
+            mPeerPicked = null;
+        }
+
         mPeers.remove(id);
         mAdapter.notifyDataSetChanged();
     }
 
     private void handleItemClick(String id) {
+        final Intent intent = getIntent();
+        if (Intent.ACTION_SEND.equals(intent.getAction())) {
+            final Uri uri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+            if (uri != null) {
+                sendFile(id, uri);
+            }
+        } else {
+            mPeerPicked = id;
+            Intent requestIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            requestIntent.addCategory(Intent.CATEGORY_OPENABLE);
+            requestIntent.setType("*/*");
+            startActivityForResult(Intent.createChooser(requestIntent, "File"), REQUEST_PICK);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_PICK) {
+            if (resultCode == RESULT_OK && mPeerPicked != null && data != null) {
+                sendFile(mPeerPicked, data.getData());
+            } else {
+                finish();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void sendFile(String id, Uri uri) {
+        Cursor cursor = getContentResolver().query(
+                uri, null, null, null, null);
+
+        if (cursor == null) {
+            return;
+        }
+
+        final int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+
+        cursor.moveToFirst();
+
+        final String name = cursor.getString(nameIndex);
+
+        cursor.close();
+
+        mAirDropManager.ask(id, name, new AirDropManager.AskCallback() {
+            @Override
+            public void onAskResult(boolean accepted) {
+                Log.d(TAG, "Accepted: " + accepted);
+            }
+        });
     }
 
     private class PeersAdapter extends RecyclerView.Adapter<PeersAdapter.ViewHolder> {
