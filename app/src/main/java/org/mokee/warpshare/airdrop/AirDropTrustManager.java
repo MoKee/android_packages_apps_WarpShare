2b9a2275e1cfb597631ced878ff585607e7a41e3
XiNGRZ <chenxingyu92@gmail.com>
2019-08-29 17:03:53 +0800

    [WIP] Implement AirDrop Server
    Change-Id: I48bdc6ed35e2c2ca727e6b793983e84db30215c5

--- app/src/main/java/org/mokee/warpshare/airdrop/AirDropTrustManager.java
+++ app/src/main/java/org/mokee/warpshare/airdrop/AirDropTrustManager.java
@@ -2,6 +2,8 @@ package org.mokee.warpshare.airdrop;
 
 import android.content.Context;
 
+import androidx.annotation.RawRes;
+
 import org.mokee.warpshare.R;
 
 import java.io.IOException;
@@ -10,11 +12,12 @@ import java.security.GeneralSecurityException;
 import java.security.KeyStore;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateFactory;
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.Locale;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
@@ -22,17 +25,33 @@ import javax.net.ssl.X509TrustManager;
 
 class AirDropTrustManager {
 
+    private static final char[] PASSWORD = "password".toCharArray();
+
     private X509TrustManager mTrustManager;
     private SSLSocketFactory mSslSocketFactory;
+    private SSLServerSocketFactory mSslServerSocketFactory;
 
     AirDropTrustManager(Context context) {
         try {
-            final InputStream ca = context.getResources().openRawResource(R.raw.apple_root_ca);
-            mTrustManager = trustManagerForCertificates(ca);
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, new TrustManager[]{mTrustManager}, null);
-            mSslSocketFactory = sslContext.getSocketFactory();
-        } catch (GeneralSecurityException e) {
+            final KeyStore keyStore = loadKeyStore(context, R.raw.keystore);
+            loadCertificates(context, R.raw.apple_root_ca, keyStore);
+            loadCertificates(context, R.raw.opendrop, keyStore);
+
+            final KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            keyManagerFactory.init(keyStore, PASSWORD);
+
+            final TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init(keyStore);
+
+            final TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+            mTrustManager = (X509TrustManager) trustManagers[0];
+
+            final SSLContext ctx = SSLContext.getInstance("TLS");
+            ctx.init(keyManagerFactory.getKeyManagers(), trustManagers, null);
+
+            mSslSocketFactory = ctx.getSocketFactory();
+            mSslServerSocketFactory = ctx.getServerSocketFactory();
+        } catch (GeneralSecurityException | IOException e) {
             throw new RuntimeException(e);
         }
     }
@@ -45,46 +64,27 @@ class AirDropTrustManager {
         return mSslSocketFactory;
     }
 
-    private X509TrustManager trustManagerForCertificates(InputStream in)
-            throws GeneralSecurityException {
-        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-        Collection<? extends Certificate> certificates = certificateFactory.generateCertificates(in);
-        if (certificates.isEmpty()) {
-            throw new IllegalArgumentException("expected non-empty set of trusted certificates");
-        }
-
-        // Put the certificates a key store.
-        char[] password = "password".toCharArray(); // Any password will work.
-        KeyStore keyStore = newEmptyKeyStore(password);
-        int index = 0;
-        for (Certificate certificate : certificates) {
-            String certificateAlias = Integer.toString(index++);
-            keyStore.setCertificateEntry(certificateAlias, certificate);
-        }
-
-        // Use it to build an X509 trust manager.
-        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(
-                KeyManagerFactory.getDefaultAlgorithm());
-        keyManagerFactory.init(keyStore, password);
-        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
-                TrustManagerFactory.getDefaultAlgorithm());
-        trustManagerFactory.init(keyStore);
-        TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
-        if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
-            throw new IllegalStateException("Unexpected default trust managers:"
-                    + Arrays.toString(trustManagers));
-        }
-        return (X509TrustManager) trustManagers[0];
+    SSLServerSocketFactory getSslServerSocketFactory() {
+        return mSslServerSocketFactory;
     }
 
-    private KeyStore newEmptyKeyStore(char[] password) throws GeneralSecurityException {
-        try {
-            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            InputStream in = null; // By convention, 'null' creates an empty key store.
-            keyStore.load(in, password);
-            return keyStore;
-        } catch (IOException e) {
-            throw new AssertionError(e);
+    private KeyStore loadKeyStore(Context context, @RawRes int id) throws GeneralSecurityException, IOException {
+        final InputStream jks = context.getResources().openRawResource(id);
+        final KeyStore keyStore = KeyStore.getInstance("pkcs12");
+        keyStore.load(jks, PASSWORD);
+        return keyStore;
+    }
+
+    private void loadCertificates(Context context, @RawRes int id, KeyStore keyStore) throws GeneralSecurityException, IOException {
+        try (final InputStream ca = context.getResources().openRawResource(id)) {
+            final CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+            final Collection<? extends Certificate> certificates = certificateFactory.generateCertificates(ca);
+
+            int index = 0;
+            for (Certificate certificate : certificates) {
+                String certificateAlias = String.format(Locale.US, "%d-%d", id, index++);
+                keyStore.setCertificateEntry(certificateAlias, certificate);
+            }
         }
     }
 
