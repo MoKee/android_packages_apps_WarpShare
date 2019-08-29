6651be231b2ea3c452c7f716991f1fe7b5b26137
XiNGRZ <chenxingyu92@gmail.com>
2019-08-29 13:56:38 +0800

    Use bottom sheet for file sharing

--- app/src/main/java/org/mokee/fileshare/ShareActivity.java
+++ app/src/main/java/org/mokee/fileshare/ShareActivity.java
@@ -2,33 +2,17 @@ package org.mokee.fileshare;
 
 import android.os.Bundle;
 
-import org.mokee.fileshare.airdrop.AirDropManager;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class ShareActivity extends BasePeersActivity {
+public class ShareActivity extends AppCompatActivity {
 
     private static final String TAG = "ShareActivity";
 
     @Override
-    protected int provideContentViewId() {
-        return R.layout.activity_share;
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle(R.string.select_to_share);
-    }
-
-    @Override
-    protected void handleItemClick(AirDropManager.Peer peer) {
-        super.handleItemClick(peer);
-        sendFile(peer, getIntent().getClipData());
-    }
-
-    @Override
-    protected void handleSendSucceed() {
-        super.handleSendSucceed();
-        finish();
+        ShareBottomSheetFragment shareFragment = new ShareBottomSheetFragment();
+        shareFragment.show(getSupportFragmentManager(), shareFragment.getTag());
     }
 
 }
