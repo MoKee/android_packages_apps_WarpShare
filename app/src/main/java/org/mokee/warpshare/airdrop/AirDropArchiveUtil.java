ed4d7649fc4e9f83a3aba6d9a49dd9d61f6c5cdf
XiNGRZ <chenxingyu92@gmail.com>
2019-09-07 03:15:33 +0800

    WarpShare: Compress files in stream
    Change-Id: Iecf12e68c7b4bdb9e25f7d2415efe3b349c343ed

--- app/src/main/java/org/mokee/warpshare/airdrop/AirDropArchiveUtil.java
+++ app/src/main/java/org/mokee/warpshare/airdrop/AirDropArchiveUtil.java
@@ -9,7 +9,9 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
 
-import okio.Buffer;
+import okio.BufferedSource;
+import okio.ByteString;
+import okio.Okio;
 
 import static org.apache.commons.compress.archivers.cpio.CpioConstants.C_IRGRP;
 import static org.apache.commons.compress.archivers.cpio.CpioConstants.C_IROTH;
@@ -24,16 +26,22 @@ class AirDropArchiveUtil {
         try (final GzipCompressorOutputStream gzip = new GzipCompressorOutputStream(output);
              final CpioArchiveOutputStream cpio = new CpioArchiveOutputStream(gzip, FORMAT_OLD_ASCII)) {
             for (ResolvedUri uri : uris) {
-                final Buffer buffer = new Buffer();
-                buffer.readFrom(uri.stream());
-                final byte[] content = buffer.readByteArray();
-                buffer.close();
-
-                final CpioArchiveEntry entry = new CpioArchiveEntry(FORMAT_OLD_ASCII, uri.path(), content.length);
+                final CpioArchiveEntry entry = new CpioArchiveEntry(FORMAT_OLD_ASCII, uri.path());
                 entry.setMode(C_ISREG | C_IRUSR | C_IWUSR | C_IRGRP | C_IROTH);
 
-                cpio.putArchiveEntry(entry);
-                cpio.write(content);
+                final BufferedSource source = Okio.buffer(Okio.source(uri.stream()));
+                final long size = uri.size();
+                if (size == -1) {
+                    final ByteString content = source.readByteString();
+                    entry.setSize(content.size());
+                    cpio.putArchiveEntry(entry);
+                    content.write(cpio);
+                } else {
+                    entry.setSize(size);
+                    cpio.putArchiveEntry(entry);
+                    source.readAll(Okio.sink(cpio));
+                }
+
                 cpio.closeArchiveEntry();
             }
         }
