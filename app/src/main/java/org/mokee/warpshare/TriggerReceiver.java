646a60af6b0e71ca628a981c8f7b8ef840cd52d9
XiNGRZ <chenxingyu92@gmail.com>
2019-09-26 12:06:45 +0800

    WarpShare: Allow changing discoverability
    Change-Id: I9166c14d8b28c1f7e3636d591955ad63f6b49410

---
+++ app/src/main/java/org/mokee/warpshare/TriggerReceiver.java
@@ -0,0 +1,36 @@
+package org.mokee.warpshare;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class TriggerReceiver extends BroadcastReceiver {
+
+    public static final String TAG = "TriggerReceiver";
+
+    public static final String EXTRA_CALLBACK_INTENT = "callback";
+
+    static PendingIntent getTriggerIntent(Context context) {
+        final PendingIntent callbackIntent = ReceiverService.getTriggerIntent(context);
+
+        final Intent intent = new Intent(context, TriggerReceiver.class);
+        intent.putExtra(TriggerReceiver.EXTRA_CALLBACK_INTENT, callbackIntent);
+
+        return PendingIntent.getBroadcast(context, callbackIntent.hashCode(),
+                intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive");
+        final PendingIntent callbackIntent = intent.getParcelableExtra(EXTRA_CALLBACK_INTENT);
+        try {
+            callbackIntent.send();
+        } catch (PendingIntent.CanceledException e) {
+            Log.e(TAG, "Failed sending callback", e);
+        }
+    }
+
+}
